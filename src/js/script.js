89fbd795e4ff2698255257429eff201172570140
webdev-alexey <alexey.anasov@yandex.by>
2023-12-31 00:03:56 +0300

    анимирована 4-я секция

--- src/js/script.js
+++ src/js/script.js
@@ -111,4 +111,85 @@ function animate() {
       },
       2,
     );
+
+  // Секция 4
+  gsap.set(".training__item:first-child", {
+    xPercent: 110,
+  });
+  gsap.set(".training__item:nth-child(2)", {
+    xPercent: 120,
+  });
+  gsap.set(".training__item:nth-child(3)", {
+    xPercent: 130,
+  });
+  gsap.set(".training__item:last-child", {
+    xPercent: 140,
+  });
+  gsap.set(".training__title", {
+    xPercent: 100,
+  });
+
+  const tlResults = gsap.timeline({
+    paused: true,
+  });
+
+  tlResults
+    .from(".results__item", {
+      xPercent: 100,
+      autoAlpha: 0,
+      stagger: 1,
+    })
+    .to(
+      ".results__item:not(:last-child)",
+      {
+        xPercent: -100,
+        stagger: 1,
+      },
+      1,
+    );
+
+  const tlTraining = gsap.timeline({
+    paused: true,
+  });
+
+  tlTraining
+    .to(".results", {
+      xPercent: -100,
+    })
+    .to(
+      ".training__item",
+      {
+        xPercent: 0,
+      },
+      "<",
+    )
+    .to(
+      ".training__title",
+      {
+        xPercent: 0,
+      },
+      "<",
+    );
+
+  const tlMain = gsap.timeline();
+
+  tlMain
+    .to(tlResults, {
+      duration: 0.8,
+      progress: 1,
+    })
+    .to(tlTraining, {
+      duration: 0.6,
+      progress: 1,
+      ease: "power3.in",
+    });
+
+  ScrollTrigger.create({
+    animation: tlMain,
+    trigger: ".double-section",
+    start: "top top",
+    end: `bottom+=${tlMain.duration() * 4000} bottom`,
+    pin: true,
+    scrub: 1,
+  });
 }
